60abf6ba746085ca2aee89d6b40e4d6c9a2a6c9a
이장훈 <chaossky@naver.com>
2017-05-03 16:53:14 +0900

    gg

--- vector_class2D/vector_class2D/main.cpp
+++ vector_class2D/vector_class2D/main.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 
+template<class TT>
 class Vector2D {
 public:
-	float x_, y_;
+	TT x_, y_;
 	Vector2D()
 		:x_(0.0f), y_(0.0f)
 	{}
 
-	Vector2D(const float& _x, const float& _y)
+	Vector2D(const TT& _x, const TT& _y)
 		:x_(_x), y_(_y)
 	{}
 
+	TT getMagnitude() {
+		return sqrt(x_*x_+y_*y_)
+	}
 	void print()
 	{
 		printf("%f %f\n", x_, y_);
@@ -27,11 +31,18 @@ public:
 };
 
 void main() {
-	Vector2D my_vector(1.1f, 2.2f);
+	Vector2D<float> my_vector(1.1f, 2.2f);
+	Vector2D<double> my_double(3.3, 4.4);
+	Vector2D<int> my_int(5.5, 6.6);
 
-	my_vector.print();
+	// my_vector.print();
 
 	std::cout << "" << std::endl;
 	std::cout << my_vector << std::endl;
+	std::cout << my_double << std::endl;
+	std::cout << my_int << std::endl;
+
+	std::cout << my_vector.getMagnitude() << std::endl;
+
  
 }
