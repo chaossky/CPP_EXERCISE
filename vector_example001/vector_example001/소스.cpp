2cfe284cb21fa7df56465d924f2df938d9572234
이장훈 <chaossky@naver.com>
2017-05-10 19:53:20 +0900

    one more time

--- vector_example001/vector_example001/소스.cpp
+++ vector_example001/vector_example001/소스.cpp
@@ -1,14 +1,20 @@
+//include
 #include<iostream>
 #include<vector>
 #include<algorithm>
 
 using namespace std;
+
 int main() {
+	//declare vector
 	vector<double> temps;
+
+	//input vector elements
 	for (double temp; cin >> temp;)
-		if(temp=='/' ) 
+		if(temp=='/' ) // if '/' is input it terminates input.
 		{}else{ temps.push_back(temp); }
-		
+	
+	//use iterator to check the vector elements
 	vector<double>::iterator iter;
 
 	cout<<"the input data are [";
@@ -17,20 +23,25 @@ int main() {
 	}
 	cout << "]" << '\n';
 
-
+	//summation of elements
 	double sum=0;
 	for (int x : temps) { sum += x; }
 
+	//calculate average value
 	cout << "Average temperature is " << sum / temps.size() << "\n";
 
+	//sort the elements in vector
 	std::sort(temps.begin(),temps.end());
+
+	//print the sorted vector
 	cout << "the sorted data are [";
 	for (iter = temps.begin(); iter != temps.end(); ++iter) {
 		cout << ' ' << *iter;
 	}
 	cout << "]" << '\n';
 
+	//calculate median value
 	cout << "Median tempearture is " << temps[temps.size() / 2] << "\n";
 
-
+	return 0;
 }
