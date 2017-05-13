4b61e33c318ab466a83b87205e11ba6c7a5948be
이장훈 <chaossky@naver.com>
2017-05-13 10:07:27 +0900

    std::cout

---
+++ CoutSample/CoutSample.cpp
@@ -0,0 +1,18 @@
+// CoutSample.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << 10 << std::endl;
+	std::cout<< 10U << std::endl;
+	std::cout<<10.5F << std::endl;
+	std::cout << 10.5 << std::endl;
+	std::cout << 3+4 << std::endl;
+
+    return 0;
+}
+
