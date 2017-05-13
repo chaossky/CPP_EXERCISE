e1e025ca73e78463e94bb3cba952c926240e2697
이장훈 <chaossky@naver.com>
2017-05-13 15:30:25 +0900

    NewDelete

---
+++ NewDeleteSample/NewDeleteSample.cpp
@@ -0,0 +1,25 @@
+// NewDeleteSample.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//인스턴스 만 동적으로 생성하는 경우
+	int *pData = new int;
+
+	//초기값을 기술하는 경우
+	int *pNewData = new int(10);
+	
+	*pData = 5;
+	std::cout << *pData << std::endl;
+	std::cout << *pNewData << std::endl;
+
+	delete pData;
+	delete pNewData;
+    
+}
+
