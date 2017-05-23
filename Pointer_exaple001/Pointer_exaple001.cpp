5d86a5516a40c3ab43f1a135983faa0045ae6053
이장훈 <chaossky@naver.com>
2017-05-23 17:48:46 +0900

    포인터연산

--- Pointer_exaple001/Pointer_exaple001.cpp
+++ Pointer_exaple001/Pointer_exaple001.cpp
@@ -6,14 +6,33 @@
 using namespace std;
 
 int _tmain(int argc,_TCHAR* argv[])
-{
-	int a = 5;
+{/*
+ int a = 5;
 	int *p1;
 	p1 = &a;
 	cout << "a의 값은" << a << endl;
 	cout << "p1의 값은" << p1 << endl;
-	cout << "a의 주소 값은 " << &a << endl;
-	cout << "포인터 P1의 값은 " << *p1 << endl;
+	cout << "&a is pointer to the address store the value " << &a << endl;
+	cout << "p1 value pointeed by a " << *p1 << endl;
+	*/
+	//Declare array whose size is 5
+	int numbers[5];
+	int * pointer1;
+
+	pointer1 = numbers;
+    *pointer1 = 10;
+	pointer1++;
+	*pointer1 = 20;
+	pointer1 = &numbers[2];
+	*pointer1 = 30;
+	pointer1 = numbers + 3;
+	*pointer1 = 40;
+	pointer1 = numbers;
+	*(pointer1 + 4) = 50;
+
+	for (int n = 0; n < 5; n++) {
+		cout << numbers[n] <<",";
+	}
 
     return 0;
 }
